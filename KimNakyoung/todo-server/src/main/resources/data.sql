6829d81b2ef0c73b3f949ef3c78f8731958dcac8
nnieun <abocado216@naver.com>
2025-02-25 12:49:13 +0900

    새로운 할일 추가 API 구현

---
+++ KimNakyoung/todo-server/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO task (title, description, status, due_date)
+VALUES ('인텔리제이 강의듣기', 'ch01 끝내기', 'in_progress', DATEADD('DAY', 1, CURRENT_TIMESTAMP));
+
+INSERT INTO task (title, description, status, due_date)
+VALUES ('인텔리제이 강의듣기', 'ch01 끝내기', 'in_progress', DATEADD('DAY', 1, CURRENT_TIMESTAMP));
+
+-- H2는 대소문자 구별
