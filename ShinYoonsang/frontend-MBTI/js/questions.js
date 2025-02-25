d65d3ddae5e41452f68a37aaa8737832bf6a6494
Dsys1129 <dsys1129@gmail.com>
2025-02-25 18:53:03 +0900

    Front End - MBTI 강의 수강중

---
+++ ShinYoonsang/frontend-MBTI/js/questions.js
@@ -0,0 +1,15 @@
+import { questions} from '/js/data.js'
+
+const progressValueEl = document.querySelector('.progress .value')
+const numberEl = document.querySelector('.number')
+const questionEl = document.querySelector('.question')
+const choice1El = document.querySelector('.choice1')
+const choice2El = document.querySelector('.choice2')
+
+let currentNumber = 0
+let mbti = ''
+
+function renderQuestion() {
+  const question = questions[0]
+}
+
