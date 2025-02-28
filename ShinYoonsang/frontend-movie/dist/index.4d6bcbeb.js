5ea45a8fd4efc4e47d660fd501bc374ee4ac7fa3
Dsys1129 <dsys1129@gmail.com>
2025-02-28 14:44:51 +0900

    컴포넌트 - 조건과 반복

--- ShinYoonsang/frontend-movie/dist/index.4d6bcbeb.js
+++ ShinYoonsang/frontend-movie/dist/index.4d6bcbeb.js
@@ -640,21 +640,29 @@ class App extends (0, _heropy.Component) {
     constructor(){
         super({
             state: {
-                inputText: ''
+                fruits: [
+                    {
+                        name: 'Apple',
+                        price: 1000
+                    },
+                    {
+                        name: 'Banana',
+                        price: 2000
+                    },
+                    {
+                        name: 'Cherry',
+                        price: 3000
+                    }
+                ]
             }
         });
     }
     render() {
-        this.el.classList.add('search');
-        this.el.innerHTML = `<input /><button>Click!</button>`;
-        const inputEl = this.el.querySelector('input');
-        inputEl.addEventListener('input', ()=>{
-            this.state.inputText = inputEl.value;
-        });
-        const buttonEl = this.el.querySelector('button');
-        buttonEl.addEventListener('click', ()=>{
-            console.log(this.state.inputText);
-        });
+        console.log(this.state.fruits);
+        this.el.innerHTML = /* html */ `<h1>Fruits</h1>
+    <ul>
+      ${this.state.fruits.filter((fruit)=>fruit.price < 3000).map((fruit)=>`<li>${fruit.name}</li>`).join('')}
+    </ul>`;
     }
 }
 exports.default = App;
