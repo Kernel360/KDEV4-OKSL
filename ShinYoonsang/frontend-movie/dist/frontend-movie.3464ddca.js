7cb49bff6644444de1b224a420365393ebd7e4dc
Dsys1129 <dsys1129@gmail.com>
2025-02-28 17:10:30 +0900

    컴포넌트 - 해시 라우터 관리

--- ShinYoonsang/frontend-movie/dist/frontend-movie.3464ddca.js
+++ ShinYoonsang/frontend-movie/dist/frontend-movie.3464ddca.js
@@ -142,14 +142,14 @@
       this[globalName] = mainExports;
     }
   }
-})({"1Fqy1":[function(require,module,exports,__globalThis) {
+})({"fgRDK":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
-module.bundle.HMR_BUNDLE_ID = "bed887d14d6bcbeb";
+module.bundle.HMR_BUNDLE_ID = "0907ca6d3464ddca";
 "use strict";
 /* global HMR_HOST, HMR_PORT, HMR_ENV_HASH, HMR_SECURE, HMR_USE_SSE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
@@ -595,173 +595,8 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
     }
 }
 
-},{}],"gLLPy":[function(require,module,exports,__globalThis) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-var _app = require("./App");
-var _appDefault = parcelHelpers.interopDefault(_app);
-var _index = require("./routes/index");
-var _indexDefault = parcelHelpers.interopDefault(_index);
-const root = document.querySelector('#root');
-root.append(new (0, _appDefault.default)().el);
-(0, _indexDefault.default)();
+},{}],"j4kuM":[function(require,module,exports,__globalThis) {
 
-},{"./App":"2kQhy","./routes/index":"3L9mC","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"2kQhy":[function(require,module,exports,__globalThis) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-var _heropy = require("./core/heropy");
-var _theHeader = require("./components/TheHeader");
-var _theHeaderDefault = parcelHelpers.interopDefault(_theHeader);
-class App extends (0, _heropy.Component) {
-    render() {
-        const routerView = document.createElement('router-view');
-        this.el.append(new (0, _theHeaderDefault.default)().el, routerView);
-    }
-}
-exports.default = App;
+},{}]},["fgRDK","j4kuM"], "j4kuM", "parcelRequire94c2")
 
-},{"./core/heropy":"57bZf","./components/TheHeader":"3Cyq4","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"57bZf":[function(require,module,exports,__globalThis) {
-///// Component /////
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-parcelHelpers.export(exports, "Component", ()=>Component);
-parcelHelpers.export(exports, "createRouter", ()=>createRouter);
-class Component {
-    constructor(payload = {}){
-        // tagName = null 일 시 기본값 'div'
-        const { tagName = 'div', state = {}, props = {} } = payload;
-        this.el = document.createElement(tagName);
-        this.state = state;
-        this.props = props;
-        this.render();
-    }
-    render() {}
-}
-// Router //
-function routeRender(routes) {
-    // URL에 해시가 없으면 해시 추가
-    if (!location.hash) // 히스토리 남기지 않고 페이지 이동
-    history.replaceState(null, '', '/#/');
-    const routerView = document.querySelector('router-view');
-    // http://localhost:1234/#/about
-    // #about
-    const [hash, queryString = ''] = location.hash.split('?');
-    // a=123&b=456
-    // ['a=123', 'b=456']
-    const query = queryString.split('&').reduce((acc, cur)=>{
-        const [key, value] = cur.split('=');
-        // {a :'123', b:'456'}
-        acc[key] = value;
-        return acc;
-    }, {});
-    history.replaceState(query, '', '');
-    const currentRoute = routes.find((route)=>new RegExp(`${route.path}/?$`).test(hash));
-    routerView.innerHTML = '';
-    routerView.append(new currentRoute.Component().el);
-    window.scrollTo(0, 0);
-}
-function createRouter(routes) {
-    return function() {
-        window.addEventListener('popstate', ()=>{
-            routeRender(routes);
-        });
-        routeRender(routes);
-    };
-}
-
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gkKU3":[function(require,module,exports,__globalThis) {
-exports.interopDefault = function(a) {
-    return a && a.__esModule ? a : {
-        default: a
-    };
-};
-exports.defineInteropFlag = function(a) {
-    Object.defineProperty(a, '__esModule', {
-        value: true
-    });
-};
-exports.exportAll = function(source, dest) {
-    Object.keys(source).forEach(function(key) {
-        if (key === 'default' || key === '__esModule' || Object.prototype.hasOwnProperty.call(dest, key)) return;
-        Object.defineProperty(dest, key, {
-            enumerable: true,
-            get: function() {
-                return source[key];
-            }
-        });
-    });
-    return dest;
-};
-exports.export = function(dest, destName, get) {
-    Object.defineProperty(dest, destName, {
-        enumerable: true,
-        get: get
-    });
-};
-
-},{}],"3Cyq4":[function(require,module,exports,__globalThis) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-var _heropy = require("../core/heropy");
-class TheHeader extends (0, _heropy.Component) {
-    constructor(){
-        super({
-            tagName: 'header'
-        });
-    }
-    render() {
-        this.el.innerHTML = `<a href="#"/>Main!</a>
-    <a href="#/about">About!</a>`;
-    }
-}
-exports.default = TheHeader;
-
-},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"3L9mC":[function(require,module,exports,__globalThis) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-var _heropy = require("../core/heropy");
-var _home = require("./Home");
-var _homeDefault = parcelHelpers.interopDefault(_home);
-var _about = require("./About");
-var _aboutDefault = parcelHelpers.interopDefault(_about);
-exports.default = (0, _heropy.createRouter)([
-    {
-        path: '#/',
-        Component: (0, _homeDefault.default)
-    },
-    {
-        path: '#/about',
-        Component: (0, _aboutDefault.default)
-    }
-]);
-
-},{"../core/heropy":"57bZf","./Home":"0JSNG","./About":"gdB30","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"0JSNG":[function(require,module,exports,__globalThis) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-var _heropy = require("../core/heropy");
-class Home extends (0, _heropy.Component) {
-    render() {
-        this.el.innerHTML = `<h1>Home Page!</h1>
-    `;
-    }
-}
-exports.default = Home;
-
-},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gdB30":[function(require,module,exports,__globalThis) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-parcelHelpers.defineInteropFlag(exports);
-var _heropy = require("../core/heropy");
-class About extends (0, _heropy.Component) {
-    render() {
-        const { a, b, c } = history.state;
-        this.el.innerHTML = `<h1>About Page!</h1>
-    <h2>${a}</h2>
-    <h2>${b}</h2>
-    <h2>${c}</h2>
-    `;
-    }
-}
-exports.default = About;
-
-},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["1Fqy1","gLLPy"], "gLLPy", "parcelRequire94c2")
-
-//# sourceMappingURL=index.4d6bcbeb.js.map
+//# sourceMappingURL=frontend-movie.3464ddca.js.map
--- ShinYoonsang/frontend-movie/src/core/heropy.js
+++ ShinYoonsang/frontend-movie/src/core/heropy.js
@@ -1,3 +1,5 @@
+
+
 ///// Component /////
 export class Component {
   constructor(payload = {}) {
@@ -12,5 +14,47 @@ export class Component {
     this.render()
   }
   render() {
+  }
+}
+
+
+// Router //
+function routeRender(routes) {
+
+  // URL에 해시가 없으면 해시 추가
+  if (!location.hash) {
+    // 히스토리 남기지 않고 페이지 이동
+    history.replaceState(null, '', '/#/')
+  }
+  const routerView = document.querySelector('router-view')
+  // http://localhost:1234/#/about
+  // #about
+  const [hash, queryString = ''] = location.hash.split('?')
+
+  // a=123&b=456
+  // ['a=123', 'b=456']
+  const query = queryString
+  .split('&')
+  .reduce((acc, cur) => {
+    const [key, value] = cur.split('=')
+    // {a :'123', b:'456'}
+    acc[key] = value
+    return acc
+  }, {})
+
+  history.replaceState(query, '', '');
+
+  const currentRoute = routes.find(route => new RegExp(`${route.path}/?$`).test(hash))
+  routerView.innerHTML = ''
+  routerView.append(new currentRoute.Component().el)
+
+  window.scrollTo(0, 0)
+}
+export function createRouter(routes) {
+  return function() {
+    window.addEventListener('popstate', () => {
+      routeRender(routes)
+    })
+    routeRender(routes)
   }
 }
